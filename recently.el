bec3107fbabe334667ef0560d8d19a6725c1b266
10sr <8.slashes@gmail.com>
2018-12-19 16:24:11 +0900

    Update commentary

--- recently.el
+++ recently.el
@@ -38,7 +38,7 @@
 ;; Track recently opened files to visit them again.
 
 ;; M-x recently-mode:  Start recording file paths on visiting them
-;; M-x recently-show:  Display buffer to visit files again
+;; M-x recently-show:  Display buffer that shows list of recently visited files
 
 
 ;;; Code:
