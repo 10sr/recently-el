26d796e4a2b7260b38e5bbc802df023d9f539580
10sr <8.slashes@gmail.com>
2018-12-21 00:43:09 +0900

    Fix license

--- recently.el
+++ recently.el
@@ -6,20 +6,19 @@
 ;; Keywords: utility files
 ;; Package-Requires: ((cl-lib "0.5") (emacs "24"))
 
-;; This file is part of EditorConfig Emacs Plugin.
+;; This file is not part of GNU Emacs.
 
-;; EditorConfig Emacs Plugin is free software: you can redistribute it and/or
-;; modify it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or (at your
-;; option) any later version.
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
 
-;; EditorConfig Emacs Plugin is distributed in the hope that it will be useful,
+;; This file is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
-;; Public License for more details.
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
 
-;; You should have received a copy of the GNU General Public License along with
-;; EditorConfig Emacs Plugin. If not, see <https://www.gnu.org/licenses/>.
+;; For a copy of the GPL see https://www.gnu.org/licenses/gpl.txt.
 
 ;;; Commentary:
 
